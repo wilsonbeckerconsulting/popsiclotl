ddf883a328b6929b643eaca7aab84fde1bf25227
webecker25272 <wilsonebecker@gmail.com>
2025-07-08 17:22:03 -0400

    publishing to heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Route all requests to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Popsiclotl app listening at http://localhost:${port}`);
+});
